259425e296be2e8d9a439f60cc4ae340111923f4
frank chege <francischege602@gmail.com>
2024-02-27 15:00:37 +0300

    restful api

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,8 +1,18 @@
 #!/usr/bin/python3
 from api.v1.views import app_views
 from flask import jsonify
+from ..app import app
+from models.engine.db_storage import *
 
 @app_views.route('/status')
 def status():
     """return the status of the api"""
     return jsonify({"status": "OK"})
+
+@app.route('/api/v1/stats')
+def retrieve():
+    new_dict = {}
+    for obj in classes:
+        total = count(obj)
+        new_dict[obj] = total
+    return jsonify(new_dict)
