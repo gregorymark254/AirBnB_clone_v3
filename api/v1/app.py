bbd72d138c65df81099853338093925e65ab85f8
frankrazzih <francischege602@gmail.com>
2024-02-27 22:06:39 +0300

    restful api

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,6 +2,7 @@
 """
 contains an flask app
 """
+
 from flask import make_response, jsonify, Flask
 from models import storage
 from api.v1.views import app_views
@@ -9,14 +10,18 @@ import os
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+
+
 @app.teardown_appcontext
 def storage_close():
     """close the storage"""
     storage.close()
 
+
 @app.errorhandler(404)
 def err_404(error):
     return make_response(jsonify({"error": "Not found"}), 404)
+
 
 if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
